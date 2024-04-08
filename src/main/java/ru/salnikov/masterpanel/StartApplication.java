3caf1f40f7a2461f4c70caf5aa7668cbf0a42117
Aleksey <lexasalnikov@gmail.com>
2024-04-08 19:09:44 +0500

    Убрал лишнее в StartApplication.java и подправил шаблон main-page.fxml

--- src/main/java/ru/salnikov/masterpanel/StartApplication.java
+++ src/main/java/ru/salnikov/masterpanel/StartApplication.java
@@ -2,7 +2,6 @@ package ru.salnikov.masterpanel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,13 +9,10 @@ import java.io.IOException;
 
 public class StartApplication extends Application {
 
-    private Stage primaryStage;
-
     @Override
     public void start(Stage stage) throws IOException {
-        this.primaryStage = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login-panel.fxml"));
 
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login-panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("login panel");
         stage.setScene(scene);
