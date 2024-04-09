f12997a263bc7a60e6b5c7a479074f9075213e69
Aleksey <lexasalnikov@gmail.com>
2024-04-09 13:39:21 +0500

    Начал работу с таблицами

--- src/main/java/ru/salnikov/masterpanel/controlles/ZavozController.java
+++ src/main/java/ru/salnikov/masterpanel/controlles/ZavozController.java
@@ -1,12 +1,16 @@
 package ru.salnikov.masterpanel.controlles;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import ru.salnikov.masterpanel.Materials;
 import ru.salnikov.masterpanel.StartApplication;
 
 import java.io.IOException;
@@ -19,10 +23,20 @@ public class ZavozController {
     public MenuButton menuButton;
     public Button okButton;
     public TextField textFieldVes;
+    public TableView<Materials> table;
+
+    static ObservableList<Materials> materialsList;
 
     MenuItem menuItem1 = new MenuItem("Труба Incoloy 800");
     MenuItem menuItem2 = new MenuItem("Кабель б/у");
 
+    static {
+        materialsList = FXCollections.observableArrayList(
+                new Materials(1, "Incoloy 800",1250, "12-03-2024"),
+                new Materials(2, "Incoloy 800",640, "14-05-2024"),
+                new Materials(3, "Кабель б/у",2300, "22-05-2024")
+        );
+    }
     @FXML
     protected void backClick() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("main-page.fxml"));
@@ -43,6 +57,9 @@ public class ZavozController {
         System.out.println(datePicker.getValue());
         System.out.println(textFieldVes.getText());
         System.out.println(menuButton.getText());
+        menuButton.setText("Номенклатура");
+
+
     }
 
     public void incoloy800(ActionEvent actionEvent) {
